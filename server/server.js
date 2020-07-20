a36cc16dbe44a7d58b3cfba6d34c203da04611d2
mjrosenberg <mjr70@duke.edu>
2020-07-20 11:32:57 -0700

    setting up server side rendering

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require('dotenv');
 require('newrelic');
 const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 const cors = require('cors');
 const morgan = require('morgan');
 const path = require('path');
@@ -13,11 +14,29 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}))
 
-app.use(express.static(path.join(__dirname, '../dist')))
+// app.use(express.static(path.join(__dirname, '../dist')))
 
 app.get('/', (req, res) => {
   res.send('Working...')
+  // res.status(200);
 })
+
+app.use('/',createProxyMiddleware({
+  target:'http://ec2-13-52-220-253.us-west-1.compute.amazonaws.com',
+  // load balancer url for lisa
+  changeOrigin:true,
+},))
+app.use('/',createProxyMiddleware({
+  target:'http://ec2-54-67-54-160.us-west-1.compute.amazonaws.com/',
+  //load balancer url for eduardo
+  changeOrigin:true,
+},))
+app.use('/',createProxyMiddleware({
+  target:'http://ec2-54-183-54-127.us-west-1.compute.amazonaws.com',
+  // load balancer url for me
+  changeOrigin:true,
+},))
+
 
 const PORT = process.env.PORT || 4500;
 
