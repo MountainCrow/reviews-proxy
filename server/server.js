1f4d741af037eb451b2a691f838f101fb31f6fb0
mjrosenberg <mjr70@duke.edu>
2020-07-21 16:27:54 -0700

    proxy displays all three components

--- server/server.js
+++ server/server.js
@@ -14,29 +14,40 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}))
 
-// app.use(express.static(path.join(__dirname, '../dist')))
+// app.set('view engine', 'pug');
+app.use(express.static(path.join(__dirname, '../dist')));
+const lisa = createProxyMiddleware({ target: 'http://ec2-13-57-221-22.us-west-1.compute.amazonaws.com', changeOrigin:true});
+const eduardo = createProxyMiddleware({ target:'http://ec2-54-183-193-8.us-west-1.compute.amazonaws.com', changeOrigin:true});
+const max = createProxyMiddleware({target: 'http://ec2-52-53-212-33.us-west-1.compute.amazonaws.com/bundle.js', changeOrigin:true});
 
-app.get('/', (req, res) => {
-  res.send('Working...')
-  // res.status(200);
-})
+app.use('/lisa',lisa);
+app.use('/ed',eduardo);
+app.use('/max', max);
+// app.use('/', createProxyMiddleware({
+//   target: 'http://ec2-13-57-221-22.us-west-1.compute.amazonaws.com',
+//   // target:['http://ec2-13-52-220-253.us-west-1.compute.amazonaws.com', 'http://ec2-54-67-54-160.us-west-1.compute.amazonaws.com/','http://ec2-54-183-54-127.us-west-1.compute.amazonaws.com'],
+//   // load balancer url for lisa
+//   changeOrigin:true,
+//   router: {
+//     'localhost:4500': 'http://ec2-54-67-54-160.us-west-1.compute.amazonaws.com',
+//     'localhost:4500': 'http://ec2-54-183-54-127.us-west-1.compute.amazonaws.com',
+//   },
+// },))
+// app.use('/ed', createProxyMiddleware({
+//   target:'http://ec2-54-67-54-160.us-west-1.compute.amazonaws.com/',
+//   //load balancer url for eduardo
+//   changeOrigin:true,
+// },))
+// app.use('/max', createProxyMiddleware({
+//   target:'http://ec2-54-183-54-127.us-west-1.compute.amazonaws.com',
+//   // load balancer url for me
+//   changeOrigin:true,
+// },))
 
-app.use('/',createProxyMiddleware({
-  target:'http://ec2-13-52-220-253.us-west-1.compute.amazonaws.com',
-  // load balancer url for lisa
-  changeOrigin:true,
-},))
-app.use('/',createProxyMiddleware({
-  target:'http://ec2-54-67-54-160.us-west-1.compute.amazonaws.com/',
-  //load balancer url for eduardo
-  changeOrigin:true,
-},))
-app.use('/',createProxyMiddleware({
-  target:'http://ec2-54-183-54-127.us-west-1.compute.amazonaws.com',
-  // load balancer url for me
-  changeOrigin:true,
-},))
-
+// app.get('*', (req, res) => {
+//   res.render(path.join(__dirname, '../dist/index.pug'));
+//   // res.status(200);
+// })
 
 const PORT = process.env.PORT || 4500;
 
